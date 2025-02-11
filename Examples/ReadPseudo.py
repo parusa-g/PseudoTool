8bec7cd600d150d066fabd4e4f95db453acbb537
parusa-g <gian.parusa@psi.ch>
2025-02-11 14:27:26 +0100

    Uploaded the module and example scripts

---
+++ Examples/ReadPseudo.py
@@ -0,0 +1,75 @@
+import sys
+sys.path.append('../')
+from ReadPseudoXml import readxml
+import numpy as np
+import matplotlib.pyplot as plt
+import xml.etree.ElementTree as ET
+#================================================================
+def PlotData(x,y,xlabel='',ylabel='',title=''):
+    fig, ax = plt.subplots(figsize=(8,8))
+    
+    ydim = y.ndim
+    
+    if ydim == 1:
+        ax.plot(x,y)
+    elif ydim > 1:
+        for i in range(y.shape[1]):
+            ax.plot(x,y[:,i])
+    
+    if len(xlabel) > 0:
+        ax.set_xlabel(xlabel)
+        
+    if len(ylabel) > 0:
+        ax.set_ylabel(ylabel)
+        
+    if len(title) > 0:
+        ax.set_title(title)
+        
+    plt.show()
+#================================================================
+def main(xml_file):
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+    pp = readxml(root)
+    
+    # List of possible items:
+    # ---------------------------------------------------------------
+    # (1) Get the rmesh
+    rr = pp.get_data('pp_rmesh')
+    
+    # (2) Get the dr = r_{i+1/2} - r_{i-1/2}
+    dr = pp.get_data('pp_rab')
+    
+    # (3) Get the atomic local potential
+    Vloc = pp.get_data('pp_local')
+    
+    # (4) Get the Vanderbilt projectors for the non-local potential
+    pp_beta = pp.get_data('pp_beta')
+    
+    # (5) Get the Vanderbilt eigen-values
+    pp_dij = pp.get_data('pp_dij')
+    
+    # (6) Get the pseudo-wavefunctions
+    pp_pswfc = pp.get_data('pp_pswfc')
+    
+    # (7) Get the non-linear core correction
+    pp_nlcc = pp.get_data('pp_nlcc')
+    
+    # (8) Get the pseudo-atomic charge density
+    pp_rho = pp.get_data('pp_rhoatom')
+    
+    # Plotting the quantitites
+    # ---------------------------------------------------------------
+    xlabel = 'r [Bohr]'
+    
+    PlotData(rr, Vloc, xlabel, ylabel=r'V$_{loc}$ [Ha]', title='Atomic local potential')
+    PlotData(rr, pp_beta, xlabel, ylabel=r'$\beta_{\ell}$', title='Vanderbilt projectors')
+    PlotData(rr, pp_pswfc, xlabel, ylabel=r'$\phi_{\ell}$', title='Pseudo-wavefunctions')
+    PlotData(rr, pp_nlcc, xlabel, ylabel=r'$V_{nlcc}$', title='Non-linear core correction')
+    PlotData(rr, pp_rho, xlabel, ylabel=r'$\rho_{\ell}$', title='Pseudo-atomic charge density')
+#===============================================================================================
+if __name__ == '__main__':
+    xml_file = 'Mo.xml'
+    main(xml_file)
+
+
---
+++ ReadPseudoXml.py
@@ -0,0 +1,126 @@
+import sys
+import numpy as np
+import xml.etree.ElementTree as ET
+
+class readxml(object):
+    def __init__(self,root):
+        self.root = root
+        self.keys = self.get_keys()
+        self.labels = self.get_labels()
+
+    def get_keys(self):
+        keys = {}
+        # Get keys from every xml data's child
+        for i,item in enumerate(self.root):
+            keys[item.tag] = i
+
+        return keys
+        
+    def get_labels(self):
+        # Get the labels from PP header
+        k = self.keys['pp_header']
+        lbl = {}
+        for i,item in enumerate(self.root[k]):
+            lbl[item.tag] = item.text
+
+        return lbl
+    
+    def get_data(self,item_name):
+        
+        if item_name == 'pp_rmesh':
+            ii = self.keys['pp_mesh']
+            sz = int(self.labels['mesh_size'])
+
+            raw = self.root[ii][0].text.split()
+            tmp = [np.float128(s) for s in raw]
+            tmp = np.array(tmp)
+            
+        elif item_name == 'pp_rab':
+            ii = self.keys['pp_mesh']
+            sz = int(self.labels['mesh_size'])
+            
+            raw = self.root[ii][1].text.split()
+            tmp = [np.float128(s) for s in raw]
+            tmp = np.array(tmp)
+            
+        elif item_name == 'pp_dij':
+            nproj = int(self.labels['number_of_proj'])
+            ii = self.keys['pp_nonlocal']
+            raw = self.root[ii][-1].text.split()
+            tmp = [np.float128(s) for s in raw]
+            tmp = 0.5 * np.array(tmp).reshape((nproj,nproj))
+            
+        elif item_name == 'pp_beta':
+            ii = self.keys['pp_nonlocal']
+            size = int(self.labels['mesh_size'])
+            nproj = int(self.labels['number_of_proj'])
+            tmp = np.zeros((size,nproj))
+            for i in range(nproj):
+                raw = self.root[ii][i].text.split()
+                dat = [np.float128(s) for s in raw]
+                tmp[:,i] = np.array(dat)
+                
+        elif item_name == 'pp_pswfc':
+            ii = self.keys[item_name]
+            size = int(self.labels['mesh_size'])
+            nwf = int(self.labels['number_of_wfc'])
+            tmp = np.zeros((size,nwf))
+            for i in range(nwf):
+                raw = self.root[ii][i].text.split()
+                dat = [np.float128(s) for s in raw]
+                tmp[:,i] = np.array(dat)
+        else:
+            ii = self.keys[item_name]
+            raw = self.root[ii].text.split()
+            tmp = [np.float128(s) for s in raw]
+            tmp = np.array(tmp)
+
+        # Check dimension 
+        if item_name != 'pp_dij':
+            dim = int(self.labels['mesh_size'])
+            if tmp.shape[0] != dim:
+                raise ValueError('Dimension incorrect')
+
+        return tmp
+
+    def save_to_file(self,item_name,fname):
+
+        if item_name != 'pp_dij':
+            x = self.get_data('pp_rmesh')
+            y = self.get_data(item_name)
+            dim = y.ndim
+
+            if dim == 1:
+                y = y.reshape(-1,1)
+                fmt = ['%12.9f', '%12.9f']
+            else:
+                nn = y.shape[-1] + 1
+                fmt = []
+                for i in range(nn):
+                    fmt.append('%12.9f')
+
+            dat = np.concatenate((x,y), axis=1)
+        else:
+            dat = self.get_data(item_name)
+            fmt = []
+            nn = int(self.labels['number_of_proj'])
+            for i in range(nn):
+                fmt.append('%12.9f')
+
+        np.savetxt(fname, dat, fmt=fmt)
+# -----------------------------------------------------------------
+def main(xml_file,item):
+    # Parse xml file
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+    pp = readxml(root)
+
+    # Save the data to a file
+    file_out = item + '.dat'
+    pp.save_to_file(item,file_out)
+# -----------------------------------------------------------------
+if __name__ == '__main__':
+    # Usage: python ReadPseudoXml.py <path/to/xml_file> <pp_item>
+    # List of <pp_item>: see the examples in ReadPseudo.py
+    
+    main(sys.argv[1], sys.argv[2])
